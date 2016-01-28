859261a33b0ac25d92976ae86005b77980703dce
Julio Makdisse Saito <saitodisse@gmail.com>
2016-01-28 09:25:10 -0200

    sharedProgressBarStyle and tabindex=-1

--- app/BmiCalculator.js
+++ app/BmiCalculator.js
@@ -34,39 +34,31 @@ function BmiCalculator({DOM}) {
       const bmi = weight / (heightMeters * heightMeters);
       const bmiFormated = Math.floor(bmi * 100) / 100;
 
-      let className = 'progress-bar-success';
       let description = '';
       if (bmi < 15) {
-        className = 'progress-bar-end-of-world';
         description = 'anorexia / bulimia';
       } else if (bmi >= 15 && bmi < 18.5) {
-        className = 'progress-bar-warning';
         description = 'abaixo do peso';
       } else if (bmi >= 18.5 && bmi < 25) {
-        className = 'progress-bar-success';
         description = 'ok';
       } else if (bmi >= 25 && bmi < 30) {
-        className = 'progress-bar-warning';
         description = 'acima do peso';
       } else if (bmi >= 30 && bmi < 35) {
-        className = 'progress-bar-#BB491B';
         description = 'obesidade';
       } else if (bmi >= 35 && bmi < 40) {
-        className = 'progress-bar-danger';
         description = 'obesidade alta';
       } else if (bmi >= 40) {
-        className = 'progress-bar-end-of-world';
         description = 'obesidade mórbida';
       }
 
       return {
         bmi,
         bmiFormated,
-        className,
         description
       };
     }
-  ).debounce(250);
+  )
+  .debounce(50);
 
   const IMC_LENGTH = 28;
 
@@ -96,6 +88,19 @@ function BmiCalculator({DOM}) {
     const getColor = (index) => Math.floor(colorArray[index]);
 
     return `rgba(${getColor(0)}, ${getColor(1)}, ${getColor(2)}, ${getColor(3)})`;
+  };
+
+  const sharedProgressBarStyle = (min, max, backColor) => {
+    return {
+      height: '100%',
+      'line-height': '20px',
+      float: 'left',
+      'text-align': 'center',
+      color: '#fff',
+      'font-size': '12px',
+      width: ((max - min) / IMC_LENGTH) * 100 + '%',
+      'background-color': backColor,
+    };
   };
 
   return {
@@ -138,81 +143,27 @@ function BmiCalculator({DOM}) {
 
               div('.progress', [
                 div({
-                  style: {
-                    height: '100%',
-                    'line-height': '20px',
-                    float: 'left',
-                    'text-align': 'center',
-                    color: '#fff',
-                    'font-size': '12px',
-                    width: ((15 - 12) / IMC_LENGTH) * 100 + '%',
-                    'background-color': '#8C1212',
-                  },
+                  style: sharedProgressBarStyle(12, 15, '#8C1212'),
                   title: 'anorexia (< 15)',
                 }, [ '< 15' ]),
                 div({
-                  style: {
-                    height: '100%',
-                    'line-height': '20px',
-                    float: 'left',
-                    'text-align': 'center',
-                    color: '#fff',
-                    'font-size': '12px',
-                    width: ((18.5 - 15) / IMC_LENGTH) * 100 + '%',
-                    'background-color': '#B5A20D',
-                  },
+                  style: sharedProgressBarStyle(15, 18.5, '#B5A20D'),
                   title: 'abaixo do peso (15 - 18.5)',
                 }, [ '15 - 18.5' ]),
                 div({
-                  style: {
-                    height: '100%',
-                    'line-height': '20px',
-                    float: 'left',
-                    'text-align': 'center',
-                    color: '#fff',
-                    'font-size': '12px',
-                    width: ((25 - 18.5) / IMC_LENGTH) * 100 + '%',
-                    'background-color': 'green',
-                  },
+                  style: sharedProgressBarStyle(18.5, 25, 'green'),
                   title: 'ok (18.5 - 25)',
                 }, [ '18.5 - 25' ]),
                 div({
-                  style: {
-                    height: '100%',
-                    'line-height': '20px',
-                    float: 'left',
-                    'text-align': 'center',
-                    color: '#fff',
-                    'font-size': '12px',
-                    width: ((30 - 25) / IMC_LENGTH) * 100 + '%',
-                    'background-color': '#B5A20D',
-                  },
+                  style: sharedProgressBarStyle(25, 30, '#B5A20D'),
                   title: 'acima do peso (25 - 30)',
                 }, [ '25 - 30' ]),
                 div({
-                  style: {
-                    height: '100%',
-                    'line-height': '20px',
-                    float: 'left',
-                    'text-align': 'center',
-                    color: '#fff',
-                    'font-size': '12px',
-                    width: ((35 - 30) / IMC_LENGTH) * 100 + '%',
-                    'background-color': '#BB491B',
-                  },
+                  style: sharedProgressBarStyle(30, 35, '#BB491B'),
                   title: 'obesidade I (30 - 35)',
                 }, [ '30 - 35' ]),
                 div({
-                  style: {
-                    height: '100%',
-                    'line-height': '20px',
-                    float: 'left',
-                    'text-align': 'center',
-                    color: '#fff',
-                    'font-size': '12px',
-                    width: ((40 - 35) / IMC_LENGTH) * 100 + '%',
-                    'background-color': '#8C1212',
-                  },
+                  style: sharedProgressBarStyle(35, 40, '#8C1212'),
                   title: 'obesidade II (40 - 35)',
                 }, [ '40 - 35' ]),
               ]),
@@ -221,22 +172,59 @@ function BmiCalculator({DOM}) {
                 a({
                   href: 'https://github.com/saitodisse/cycle-webpack-IMC-example',
                   target: '_blank',
+                  attributes: {
+                    tabindex: -1,
+                  }
                 }, ['Fork me on GitHub']),
                 hr(),
                 'powered by: ',
-                a({href: 'http://cycle.js.org/', target: '_blank'}, ['Cycle.js']),
+                a({
+                  href: 'http://cycle.js.org/',
+                  target: '_blank',
+                  attributes: {
+                    tabindex: -1
+                  }
+                },
+                ['Cycle.js']),
                 ', ',
-                a({href: 'https://webpack.github.io/', target: '_blank'}, ['webpack']),
+                a({
+                  href: 'https://webpack.github.io/',
+                  target: '_blank',
+                  attributes: {
+                    tabindex: -1
+                  }
+                },
+                ['webpack']),
                 ', ',
-                a({href: 'https://babeljs.io/', target: '_blank'}, ['babel']),
+                a({
+                  href: 'https://babeljs.io/',
+                  target: '_blank',
+                  attributes: {
+                    tabindex: -1
+                  }
+                },
+                ['babel']),
                 ', ',
-                a({href: 'https://surge.sh/', target: '_blank'}, ['surge.sh']),
+                a({
+                  href: 'https://surge.sh/',
+                  target: '_blank',
+                  attributes: {
+                    tabindex: -1
+                  }
+                },
+                ['surge.sh']),
                 ', ',
-                a({href: 'https://github.com/miguelmota/rgb-scale', target: '_blank'}, ['rgb-scale']),
+                a({
+                  href: 'https://github.com/miguelmota/rgb-scale',
+                  target: '_blank',
+                  attributes: {
+                    tabindex: -1
+                  }
+                },
+                ['rgb-scale']),
               ])
             ]),
           ]),
-
         ])
       )
   };
